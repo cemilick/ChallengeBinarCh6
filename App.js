9533687328bdf63f26bf29e115500e1959738ccc
cemilick <cemilick@gmail.com>
2022-05-09 19:42:32 +0700

    add geolocation, login, and qrcode folder

---
+++ App.js
@@ -0,0 +1,46 @@
+import {View, Text, TouchableOpacity, Button} from 'react-native';
+import React, {useEffect} from 'react';
+
+import crashlytics from '@react-native-firebase/crashlytics';
+
+const onSignIn = async user => {
+  crashlytics().log('User Signed In.');
+  await Promise.all([
+    crashlytics().setUserId(user.id),
+    crashlytics().setAttribute('credits', String(user.credits)),
+    crashlytics().setAttrbite({
+      role: 'admin',
+      followers: 13,
+      email: user.email,
+      username: user.username,
+    }),
+  ]);
+};
+
+export default function App() {
+  useEffect(() => {
+    crashlytics().log('App Mounted');
+  }, []);
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          onSignIn({
+            uid: 'qwertyuiop',
+            username: 'cemilick cemill',
+            email: 'cemilick@cemil.com',
+            credits: 42,
+          });
+        }}>
+        <Text>Click to Test Sign In</Text>
+      </TouchableOpacity>
+      <Button
+        title="Test Crash"
+        onPress={() => {
+          console.log('app should crash');
+          crashlytics().crash();
+        }}
+      />
+    </View>
+  );
+}
