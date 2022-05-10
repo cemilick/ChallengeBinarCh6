2c75650b2bb70d94fbeba762ed5a107ad99a93f2
cemilick <cemilick@gmail.com>
2022-05-10 22:05:10 +0700

    add font comfortaa

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,10 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
+import Comfortaa from '../../components/Comfortaa';
 export default function Index() {
   return (
     <View>
-      <Text>Login</Text>
+      <Comfortaa>Login</Comfortaa>
     </View>
   );
 }
