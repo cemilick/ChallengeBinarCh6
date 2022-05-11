842b73172f27da5e5f87fd64696a4f8d6a98da8b
cemilick <cemilick@gmail.com>
2022-05-12 03:03:54 +0700

    fix permissions on map and add my location button

--- src/screens/Geolocation/index.js
+++ src/screens/Geolocation/index.js
@@ -1,7 +1,11 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, PermissionsAndroid} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 export default function Index() {
   const [initialPosition, setinitialPosition] = useState({
@@ -10,8 +14,7 @@ export default function Index() {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
-
-  useEffect(() => {
+  const getMyLocation = () => {
     Geolocation.getCurrentPosition(
       position => {
         const {longitude, latitude} = position.coords;
@@ -24,10 +27,24 @@ export default function Index() {
       },
       error => alert(error.message),
     );
+  };
+
+  useEffect(async () => {
+    try {
+      const permissions = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+
+      if (permissions === PermissionsAndroid.RESULTS.GRANTED) getMyLocation();
+      else alert('Please give this app access location permissions');
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
   return (
     <View style={styles.container}>
       <MapView
+        followsUserLocation={true}
         showsUserLocation={true}
         showsMyLocationButton={true}
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
@@ -40,8 +57,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
+    height: hp(100),
+    width: wp(100),
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
