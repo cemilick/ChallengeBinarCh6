03675cc6a9c44cfb20544669b70ce4bfc3036eb3
cemilick <cemilick@gmail.com>
2022-05-12 02:38:59 +0700

    add maps without permissions

--- src/screens/Geolocation/index.js
+++ src/screens/Geolocation/index.js
@@ -1,10 +1,51 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 export default function Index() {
+  const [initialPosition, setinitialPosition] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {longitude, latitude} = position.coords;
+
+        setinitialPosition({
+          ...initialPosition,
+          longitude: longitude,
+          latitude: latitude,
+        });
+      },
+      error => alert(error.message),
+    );
+  }, []);
   return (
-    <View>
-      <Text>Geolocation</Text>
+    <View style={styles.container}>
+      <MapView
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        initialRegion={initialPosition}
+      />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
